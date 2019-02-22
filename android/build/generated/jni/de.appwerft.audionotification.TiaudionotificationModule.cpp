585104641d54512e51aa2095ac3893635d7d7057
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2019-02-22 15:40:20 +0100

    androidx added to classpath

---
+++ android/build/generated/jni/de.appwerft.audionotification.TiaudionotificationModule.cpp
@@ -0,0 +1,140 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2011-2017 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+/** This code is generated, do not edit by hand. **/
+
+#include "de.appwerft.audionotification.TiaudionotificationModule.h"
+
+#include "AndroidUtil.h"
+#include "JNIUtil.h"
+#include "JSException.h"
+#include "TypeConverter.h"
+#include "V8Util.h"
+
+
+
+#include "de.appwerft.audionotification.NotificationProxy.h"
+
+#include "org.appcelerator.kroll.KrollModule.h"
+
+#define TAG "TiaudionotificationModule"
+
+using namespace v8;
+
+namespace de {
+namespace appwerft {
+namespace audionotification {
+
+
+Persistent<FunctionTemplate> TiaudionotificationModule::proxyTemplate;
+jclass TiaudionotificationModule::javaClass = NULL;
+
+TiaudionotificationModule::TiaudionotificationModule() : titanium::Proxy()
+{
+}
+
+void TiaudionotificationModule::bindProxy(Local<Object> exports, Local<Context> context)
+{
+	Isolate* isolate = context->GetIsolate();
+
+	Local<FunctionTemplate> pt = getProxyTemplate(isolate);
+
+	v8::TryCatch tryCatch(isolate);
+	Local<Function> constructor;
+	MaybeLocal<Function> maybeConstructor = pt->GetFunction(context);
+	if (!maybeConstructor.ToLocal(&constructor)) {
+		titanium::V8Util::fatalException(isolate, tryCatch);
+		return;
+	}
+
+	Local<String> nameSymbol = NEW_SYMBOL(isolate, "Tiaudionotification"); // use symbol over string for efficiency
+	MaybeLocal<Object> maybeInstance = constructor->NewInstance(context);
+	Local<Object> moduleInstance;
+	if (!maybeInstance.ToLocal(&moduleInstance)) {
+		titanium::V8Util::fatalException(isolate, tryCatch);
+		return;
+	}
+	exports->Set(nameSymbol, moduleInstance);
+}
+
+void TiaudionotificationModule::dispose(Isolate* isolate)
+{
+	LOGD(TAG, "dispose()");
+	if (!proxyTemplate.IsEmpty()) {
+		proxyTemplate.Reset();
+	}
+
+	titanium::KrollModule::dispose(isolate);
+}
+
+Local<FunctionTemplate> TiaudionotificationModule::getProxyTemplate(Isolate* isolate)
+{
+	if (!proxyTemplate.IsEmpty()) {
+		return proxyTemplate.Get(isolate);
+	}
+
+	LOGD(TAG, "TiaudionotificationModule::getProxyTemplate()");
+
+	javaClass = titanium::JNIUtil::findClass("de/appwerft/audionotification/TiaudionotificationModule");
+	EscapableHandleScope scope(isolate);
+
+	// use symbol over string for efficiency
+	Local<String> nameSymbol = NEW_SYMBOL(isolate, "Tiaudionotification");
+
+	Local<FunctionTemplate> t = titanium::Proxy::inheritProxyTemplate(isolate,
+		titanium::KrollModule::getProxyTemplate(isolate)
+, javaClass, nameSymbol);
+
+	proxyTemplate.Reset(isolate, t);
+	t->Set(titanium::Proxy::inheritSymbol.Get(isolate),
+		FunctionTemplate::New(isolate, titanium::Proxy::inherit<TiaudionotificationModule>));
+
+	// Method bindings --------------------------------------------------------
+
+	Local<ObjectTemplate> prototypeTemplate = t->PrototypeTemplate();
+	Local<ObjectTemplate> instanceTemplate = t->InstanceTemplate();
+
+	// Delegate indexed property get and set to the Java proxy.
+	instanceTemplate->SetIndexedPropertyHandler(titanium::Proxy::getIndexedProperty,
+		titanium::Proxy::setIndexedProperty);
+
+	// Constants --------------------------------------------------------------
+	JNIEnv *env = titanium::JNIScope::getEnv();
+	if (!env) {
+		LOGE(TAG, "Failed to get environment in TiaudionotificationModule");
+		//return;
+	}
+
+
+			DEFINE_INT_CONSTANT(isolate, prototypeTemplate, "NOTIFICATION_IMPORTANCE_NONE", 0);
+
+			DEFINE_INT_CONSTANT(isolate, prototypeTemplate, "NOTIFICATION_IMPORTANCE_DEFAULT", 3);
+
+			DEFINE_INT_CONSTANT(isolate, prototypeTemplate, "NOTIFICATION_IMPORTANCE_LOW", 2);
+
+			DEFINE_INT_CONSTANT(isolate, prototypeTemplate, "NOTIFICATION_IMPORTANCE_MAX", 5);
+
+			DEFINE_INT_CONSTANT(isolate, prototypeTemplate, "NOTIFICATION_IMPORTANCE_HIGHT", 4);
+
+			DEFINE_INT_CONSTANT(isolate, prototypeTemplate, "NOTIFICATION_IMPORTANCE_MIN", 1);
+
+
+	// Dynamic properties -----------------------------------------------------
+
+	// Accessors --------------------------------------------------------------
+
+	return scope.Escape(t);
+}
+
+// Methods --------------------------------------------------------------------
+
+// Dynamic property accessors -------------------------------------------------
+
+
+} // audionotification
+} // appwerft
+} // de
