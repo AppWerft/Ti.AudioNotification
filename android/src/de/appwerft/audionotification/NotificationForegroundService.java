666fabd9ff27a0342cace181db330f4f0971ccc4
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2019-02-22 13:46:35 +0100

    androidx added to classpath

--- android/src/de/appwerft/audionotification/NotificationForegroundService.java
+++ android/src/de/appwerft/audionotification/NotificationForegroundService.java
@@ -21,6 +21,7 @@ import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
 import android.util.Log;
+import androidx.core.app.NotificationCompat;
 
 public class NotificationForegroundService extends Service {
 	private static final String PACKAGE_NAME = TiApplication.getInstance().getPackageName();
