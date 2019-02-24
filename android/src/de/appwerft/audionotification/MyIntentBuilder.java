69fd81fdc5c452ca04e18bd67bb53fd557f60132
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2019-02-24 18:24:01 +0100

    next

--- android/src/de/appwerft/audionotification/MyIntentBuilder.java
+++ android/src/de/appwerft/audionotification/MyIntentBuilder.java
@@ -1,5 +1,40 @@
 package de.appwerft.audionotification;
 
-public class MyIntentBuilder {
+import android.content.Context;
+import android.content.Intent;
 
+public class MyIntentBuilder{
+	Context ctx;
+	String message;
+	int commandId;
+    public static MyIntentBuilder getInstance(Context context) {
+        return new MyIntentBuilder(context);
+    }
+ 
+    public MyIntentBuilder(Context context) {
+        this.ctx = context;
+    }
+ 
+    public MyIntentBuilder setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+ 
+    public MyIntentBuilder setCommand(int command) {
+        this.commandId = command;
+        return this;
+    }
+ 
+    public Intent build() {
+        
+        Intent intent = new Intent(ctx, NotificationForegroundService.class);
+        //if (commandId != Command.INVALID) {
+            intent.putExtra(Constants.KEY.COMMAND, commandId);
+        //}
+        if (message != null) {
+            intent.putExtra(Constants.KEY.MESSAGE, message);
+        }
+        return intent;
+    }
+ 
 }
